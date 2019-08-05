65cd6498388951aecb475369fafa9d86167b7cf9
Robert Fritzen <rfritzen@phantomdev.net>
2019-08-05 12:42:59 -0500

    Add python variable

--- dask_jobqueue/cobalt.py
+++ dask_jobqueue/cobalt.py
@@ -22,7 +22,7 @@ class CobaltCluster(JobQueueCluster):
 	submit_command = "qsub --mode script"
 	cancel_command = "qdel"
 
-	def __init__(self, queue=None, project=None, walltime=None, ncpus=None, job_extra=None, host=None, log_directory=None, config_name="cobalt", **kwargs):
+	def __init__(self, queue=None, project=None, walltime=None, ncpus=None, job_extra=None, host=None, log_directory=None, config_name="cobalt", python=sys.executable, **kwargs):
 		if queue is None:
 			queue = dask.config.get("jobqueue.%s.queue" % config_name)
 		if project is None:
